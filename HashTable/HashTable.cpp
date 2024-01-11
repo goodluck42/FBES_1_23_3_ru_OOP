adc4a01334b06702aa1a864bc76967bb53b89347
Alex <xrlogic1@gmail.com>
2024-01-11 08:16:59 -0800

    *

---
+++ HashTable/HashTable.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <functional>
+
+int main(int argc, char* argv[])
+{
+    std::hash<std::string> LHashFn;
+
+    std::string LName1 = "John";
+    std::string LName2 = "Samir";
+    std::string LName3 = "Amin";
+
+    int LBuckets = 12;
+    
+    std::cout << LHashFn(LName1) % LBuckets << '\n';
+    std::cout << LHashFn(LName2) % LBuckets << '\n';
+    std::cout << LHashFn(LName3) % LBuckets << '\n';
+
+
+    
+    
+    return 0;
+}
