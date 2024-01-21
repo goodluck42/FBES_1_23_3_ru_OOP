c3a9e48306da82b9355f8419e89cf2896f8e38bf
Alex <xrlogic1@gmail.com>
2024-01-21 04:33:16 -0800

    *

--- UniquePointer/UniquePointer/UniquePointer.cpp
+++ UniquePointer/UniquePointer/UniquePointer.cpp
@@ -42,13 +42,15 @@ private:
 int main(int argc, char* argv[])
 {
     UniquePtr<int[]> data{new int[32]};
-    UniquePtr<int> data2{new int};
+    UniquePtr<int> data2{new int{1337}};
 
+    std::cout << *data2 << '\n'; // shows 1337
+    
     UniquePtr<User> userPtr{new User};
 
-    std::cout << (*userPtr).Login << '\n';
-    std::cout << userPtr->Login << '\n';
-    std::cout << userPtr->Password << '\n';
+    std::cout << (*userPtr).Login << '\n'; // shows login
+    std::cout << userPtr->Login << '\n'; // shows login
+    std::cout << userPtr->Password << '\n'; // shows password
     
     return 0;
 }
