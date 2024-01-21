c6375ab478dcf926a0b2fff9cb9951aaef822285
Alex <xrlogic1@gmail.com>
2024-01-21 05:47:51 -0800

    *

--- UniquePointer/UniquePointer/UniquePointer.cpp
+++ UniquePointer/UniquePointer/UniquePointer.cpp
@@ -21,29 +21,44 @@ public:
         
     }
 
+    UniquePtr(const UniquePtr&) = delete;
+    
     UniquePtr(UniquePtr&& AOther)
     {
         // ...
     }
 
-    void operator*()
+    T& operator*()
     {
-        // ...
+        return *MData;
     }
 
-    void operator->()
+    const T& operator*() const
     {
-        // ...
+        return *MData;
+    }
+
+    T* operator->()
+    {
+        return MData;
     }
 private:
     T* MData;
 };
+
+void foo(UniquePtr<int> APtr)
+{}
 
 int main(int argc, char* argv[])
 {
     UniquePtr<int[]> data{new int[32]};
     UniquePtr<int> data2{new int{1337}};
 
+    foo(data2);
+    
+    data[0] = 90;
+    data[1] = 91;
+    
     std::cout << *data2 << '\n'; // shows 1337
     
     UniquePtr<User> userPtr{new User};
