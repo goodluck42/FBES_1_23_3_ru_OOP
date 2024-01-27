c8ef0e70a4bf9584113c580a8467d6154ec9cb0e
Alex <xrlogic1@gmail.com>
2024-01-27 11:37:16 +0400

    *

--- UniquePointer/UniquePointer/UniquePointer.cpp
+++ UniquePointer/UniquePointer/UniquePointer.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory>
 
 
 class User
@@ -51,21 +52,33 @@ void foo(UniquePtr<int> APtr)
 
 int main(int argc, char* argv[])
 {
-    UniquePtr<int[]> data{new int[32]};
-    UniquePtr<int> data2{new int{1337}};
+    // UniquePtr<int[]> data{new int[32]};
+    // UniquePtr<int> data2{new int{1337}};
+    //
+    // foo(data2);
+    //
+    // data[0] = 90;
+    // data[1] = 91;
+    //
+    // std::cout << *data2 << '\n'; // shows 1337
+    //
+    // UniquePtr<User> userPtr{new User};
+    //
+    // std::cout << (*userPtr).Login << '\n'; // shows login
+    // std::cout << userPtr->Login << '\n'; // shows login
+    // std::cout << userPtr->Password << '\n'; // shows password
 
-    foo(data2);
-    
-    data[0] = 90;
-    data[1] = 91;
-    
-    std::cout << *data2 << '\n'; // shows 1337
-    
-    UniquePtr<User> userPtr{new User};
+    std::shared_ptr<int> obj {new int};
 
-    std::cout << (*userPtr).Login << '\n'; // shows login
-    std::cout << userPtr->Login << '\n'; // shows login
-    std::cout << userPtr->Password << '\n'; // shows password
+    std::cout << obj.use_count() << '\n'; // 1
+    
+    {
+        std::shared_ptr<int> obj2 {obj};
+        std::cout << obj.use_count() << '\n'; // 2
+        std::cout << obj2.use_count() << '\n'; // 2
+    }
+    
+    std::cout << obj.use_count() << '\n'; // 1
     
     return 0;
 }
